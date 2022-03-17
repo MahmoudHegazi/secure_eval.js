0ffb1f7fdc39c496c5515ee1e5de429dc918a9c2
Mahmoud Magdy Hegazi <55125302+MahmoudHegazi@users.noreply.github.com>
2022-03-17 09:54:36 +0200

    Update secure_eval.js

--- secure_eval.js
+++ secure_eval.js
@@ -1,11 +1,16 @@
 let x = 5;
-let y = 10;
+let y = 454545454545454;
+// as u can see this example how u contain case and close any possible if ur function
+// will use eval small like this and can covered so u can use this more secure than //alert
 function secureEval(hack_string){
   // 0 risk eval calculator
   const hackerMsg = 'Sorry u can not hack my systems';
   const whiteList = ['',' ', 'x', 'y','+','*','/','-'];
+  const operators = ['+', '*', '/', '-'];
   let x_length = 0;
   let y_length = 0;
+  let op_length = 0;
+  let space_len = 0;
   for (let i=0; i<hack_string.length; i++){  
     if (hack_string[i] === 'x'){
        x_length += 1;
@@ -13,8 +18,14 @@ function secureEval(hack_string){
     if (hack_string[i] === 'y'){
        y_length += 1;
     }
+    if (hack_string[i] == ' '){    
+       space_len += 1;
+    }
+    if (operators.includes(hack_string[i])){    
+       op_length += 1;
+    }
     
-    if (x_length > 1 || y_length > 1 || !whiteList.includes(hack_string[i])){
+    if (x_length > 1 || y_length > 1 || op_length > 1 || space_len > 2 || !whiteList.includes(hack_string[i])){
       return hackerMsg;
       break;
     }
@@ -22,4 +33,4 @@ function secureEval(hack_string){
   return eval(hack_string);
 }
 
-document.getElementById("demo").innerHTML = secureEval('x * y');
+document.getElementById("demo").innerHTML = secureEval('x / y');
