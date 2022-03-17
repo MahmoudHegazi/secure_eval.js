e5526f9a72ece9368c06f9ab459232b8c49872cf
Mahmoud Magdy Hegazi <55125302+MahmoudHegazi@users.noreply.github.com>
2022-03-17 09:44:44 +0200

    Update secure_eval.js

--- secure_eval.js
+++ secure_eval.js
@@ -1,12 +1,25 @@
+let x = 5;
+let y = 10;
 function secureEval(hack_string){
   // 0 risk eval calculator
+  const hackerMsg = 'Sorry u can not hack my systems';
   const whiteList = ['',' ', 'x', 'y','+','*','/','-'];
-  for (let i=0; i<hack_string.length; i++){
-    if (!whiteList.includes(hack_string[i])){
-      return 'Sorry u can not hack my systems';
+  let x_length = 0;
+  let y_length = 0;
+  for (let i=0; i<hack_string.length; i++){  
+    if (hack_string[i] === 'x'){
+       x_length += 1;
+    }
+    if (hack_string[i] === 'y'){
+       y_length += 1;
+    }
+    
+    if (x_length > 1 || y_length > 1 || !whiteList.includes(hack_string[i])){
+      return hackerMsg;
+      break;
     }
   }
   return eval(hack_string);
 }
 
-document.getElementById("demo").innerHTML = secureEval('x * y; alert("hacked")');
+document.getElementById("demo").innerHTML = secureEval('x * y');
